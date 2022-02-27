4609c0ef40e0504583cf9d26c2511c5ca27cda24
Mdw503 <100480654+Mdw503@users.noreply.github.com>
2022-02-27 11:21:16 +0700

    Update 250tes.js

--- 250tes.js
+++ 250tes.js
@@ -1,10 +1,10 @@
-<script type="text/javascript">
+&lt;script type=&quot;text/javascript&quot;&gt;
 	atOptions = {
-		'key' : '6af0e14e1a8c31b04c591ca64b02979e',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
+		&#039;k;ey&#039; : &#039;6af0e14e1a8c31b04c591ca64b02979e&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/6af0e14e1a8c31b04c591ca64b02979e/invoke.js"></scr' + 'ipt>');
-</script>
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.highconvertingformats.com/6af0e14e1a8c31b04c591ca64b02979e/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
+&lt;/script&gt
