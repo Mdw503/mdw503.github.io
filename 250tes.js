665d4cbe571abbdad1659b56560ce930300f8981
Mdw503 <100480654+Mdw503@users.noreply.github.com>
2022-02-27 11:27:45 +0700

    Add files via upload

---
+++ 250tes.js
@@ -0,0 +1,10 @@
+<script type=&lt;script type=&quot;text&lt;script type=&quot;text/javascript&quot;&gt;
+	atOptions = {
+		&#039;k;ey&#039; : &#039;6af0e14e1a8c31b04c591ca64b02979e&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
+	};
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.highconvertingformats.com/6af0e14e1a8c31b04c591ca64b02979e/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
+&lt;/script&gt
